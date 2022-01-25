2363fea8dee477df2def7c4df0f96aebf8333701
guillaume perroy <gui.perroy@gmail.com>
2022-01-25 11:16:26 +0100

    exercice 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { statistique } from 'src/models/model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'AngEval-Gui-PERROY';
+  // title = 'AngEval-Gui-PERROY';
+  stat1: statistique = {
+    id: 'cfa35330-7db6-11ec',
+    titre: 'la jolie région du limousin',
+    valeur: '50M',
+  };
 }
